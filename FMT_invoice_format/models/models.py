990a79d658ce57d983bcad626532acb10e3c50fb
magdii <magdysala@gmail.com>
2020-03-15 14:12:03 +0300

    AFCC modification

--- FMT_invoice_format/models/models.py
+++ FMT_invoice_format/models/models.py
@@ -11,12 +11,25 @@ from odoo import models, fields, api
 #         res = super(SaleAdvancePaymentInv, self).create_invoices()
 #         # print(">>>>>>>>>>>>", res['context'])
 #         res['context'] = {'default_bayan':'bayanbayanbayanbayanbayan'}
-#         res['context']['default_landing_place'] = 'landing_place'
+#         # res['context'].update({'default_landing_place': 'landing_place'})
 #         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-#         # print(">>>>>>>>>>>>", res['context'])
+#         print(">>>>>>>>>>>>", res['context'])
 #         # print("sale_orders", res)
 #         # print("sale_orders", res['sale_orders'])
 #         return res
+#
+#     def _create_invoice(self, order, so_line, amount):
+#         # res = super(SaleAdvancePaymentInv, self)._create_invoice(self, order, so_line, amount)
+#         # print(">>>>>>>>>>>>", res['context'])
+#         # res['context'] = {'default_bayan':'bayanbayanbayanbayanbayan'}
+#         # res['context']['default_landing_place'] = 'landing_place'
+#         print(">>>>>>>>>>>>>>>>>>>second function>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+#         # print(">>>>>>>>>>>>", res)
+#         # res = super(SaleAdvancePaymentInv, self)._create_invoice(self, order, so_line, amount)
+#         print(">>>>>>>>>>>>", order)
+#         # print("sale_orders", res)
+#         # print("sale_orders", res['sale_orders'])
+#         # return res
 
 
 class SaleOrderLine(models.Model):
@@ -29,14 +42,30 @@ class SaleOrder(models.Model):
 
     date_of_prealert = fields.Date(string="Date of pre alert")
     eta_date = fields.Date(string="ETA")
-    po_date = fields.Date(string="P.O Date")
+    po_date = fields.Date(string="D.O Date")
     saddad_payment_date = fields.Date(string="Saddad Payment Date")
     consinee = fields.Many2one('consinee.sale.order', string="Consinee")
     bill_of_lading = fields.Char(string="Bill of Lading")
     bayan = fields.Char(string="Bayan")
     shipping_line = fields.Many2one('shipping.sale.order', string="Shipping line")
-    landing_place = fields.Char('Sea Port')
+    landing_place = fields.Many2one('sea.port')
     order_status_id = fields.Many2one('fmt.order.status', string="Order Status", track_visibility='onchange')
+
+    def action_view_invoice(self):
+        res = super(SaleOrder, self).action_view_invoice()
+        res['context'].update({
+            'default_date_of_prealert': self.date_of_prealert,
+            'default_eta_date': self.eta_date,
+            'default_po_date': self.po_date,
+            'default_saddad_payment_date': self.saddad_payment_date,
+            'default_consinee': self.consinee.id,
+            'default_bill_of_lading': self.bill_of_lading,
+            'default_bayan': self.bayan,
+            'default_shipping_line': self.shipping_line.id,
+            'default_landing_place': self.landing_place.id,
+            'default_order_status_id': self.order_status_id.id,
+        })
+        return res
 
 
 class AccountMoveLine(models.Model):
@@ -49,13 +78,13 @@ class AccountMove(models.Model):
 
     date_of_prealert = fields.Date(string="Date of pre alert")
     eta_date = fields.Date(string="ETA")
-    po_date = fields.Date(string="P.O Date")
+    po_date = fields.Date(string="D.O Date")
     saddad_payment_date = fields.Date(string="Saddad Payment Date")
     consinee = fields.Many2one('consinee.sale.order', string="Consinee")
     bill_of_lading = fields.Char(string="Bill of Lading")
     bayan = fields.Char(string="Bayan")
     shipping_line = fields.Many2one('shipping.sale.order', string="Shipping line")
-    landing_place = fields.Char('Sea Port')
+    landing_place = fields.Many2one('sea.port')
     order_status_id = fields.Many2one('fmt.order.status', string="Order Status", track_visibility='onchange')
 
     def get_line_with_vat(self):
@@ -85,5 +114,11 @@ class ConsineeSaleOrder(models.Model):
 
 class ShippingSaleOrder(models.Model):
     _name = 'shipping.sale.order'
+
+    name = fields.Char(required=1)
+
+
+class SeaPort(models.Model):
+    _name = 'sea.port'
 
     name = fields.Char(required=1)
